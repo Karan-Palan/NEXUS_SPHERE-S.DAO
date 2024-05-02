a11ce4d727422cb069813c52fb97df20a42eca85
KaranPalan <143683619+Karan-Palan@users.noreply.github.com>
2024-05-02 14:35:13 +0000

    Tutorial video added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -16,7 +16,7 @@ const Banner = () => {
   }
 
   const onContribute = async () => {
-    if (!!!amount || amount == '') return
+    if (!!!amount || amount === '') return
     await performContribute(amount)
     toast.success('Contribution received')
   }
@@ -27,59 +27,73 @@ const Banner = () => {
     ).length
 
   return (
-    <div className="p-8">
-      <h2 className="font-semibold text-4xl mb-5">
-        {opened()} PROPOSAL{opened() == 1 ? '' : 'S'} CURRENTLY OPENED
-      </h2>
-      <p>
-        CURRENT DAO BALANCE: <strong>{balance} Eth</strong> <br />
-        YOUR CONTRIBUTIONS:{' '}
-        <span>
-          <strong>{mybalance} Eth</strong>
-          {isStakeholder ? ', Congratulations on being a stakeholder' : null}
-        </span>
-      </p>
-      <div className="flex flex-row justify-start items-center md:w-1/3 w-full mt-4">
-        <input
-          type="number"
-          className="form-control block w-full px-3 py-1.5
-          text-base font-normaltext-gray-700
-          bg-clip-padding border border-solid border-gray-300
-          rounded transition ease-in-out m-0 shadow-md
-          focus:text-gray-500 focus:outline-none
-          dark:border-gray-500 dark:bg-transparent"
-          placeholder="e.g 2.5 Eth"
-          onChange={(e) => setAmount(e.target.value)}
-          value={amount}
-          required
-        />
-      </div>
-      <p className='mt-2'>
-        {isStakeholder
-          ? 'You can now raise proposals on this platform'
-          : 'Upon contribution of 1 ETH, You become a stakeholder'}
-      </p>
-      <div
-        className="flex flex-row justify-start items-center space-x-3 mt-4"
-        role="group"
-      >
-        <button
-          type="button"
-          className="bg-gradient-to-r from-violet-400 to-violet-800 py-2 px-3 rounded-md text-white"
-          onClick={onContribute}
+    <div className="flex flex-row justify-between p-8">
+      <div className="flex flex-col">
+        <h2 className="font-semibold text-4xl mb-5">
+          {opened()} PROPOSAL{opened() === 1 ? '' : 'S'} CURRENTLY OPENED
+        </h2>
+        <p>
+          CURRENT DAO BALANCE: <strong>{balance} Eth</strong> <br />
+          YOUR CONTRIBUTIONS:{' '}
+          <span>
+            <strong>{mybalance} Eth</strong>
+            {isStakeholder ? ', Congratulations on being a stakeholder' : null}
+          </span>
+        </p>
+        <div className="flex flex-row justify-start items-center md:w-1/3 w-full mt-4">
+          <input
+            type="number"
+            className="form-control block w-full px-3 py-1.5
+            text-base font-normaltext-gray-700
+            bg-clip-padding border border-solid border-gray-300
+            rounded transition ease-in-out m-0 shadow-md
+            focus:text-gray-500 focus:outline-none
+            dark:border-gray-500 dark:bg-transparent"
+            placeholder="e.g 2.5 Eth"
+            onChange={(e) => setAmount(e.target.value)}
+            value={amount}
+            required
+          />
+        </div>
+        <p className='mt-2'>
+          {isStakeholder
+            ? 'You can now raise proposals on this platform'
+            : 'Upon contribution of 1 ETH, You become a stakeholder'}
+        </p>
+        <div
+          className="flex flex-row justify-start items-center space-x-3 mt-4"
+          role="group"
         >
-          Contribute
-        </button>
-
-        {isStakeholder ? (
           <button
             type="button"
             className="bg-gradient-to-r from-violet-400 to-violet-800 py-2 px-3 rounded-md text-white"
-            onClick={onPropose}
+            onClick={onContribute}
           >
-            Propose
+            Contribute
           </button>
-        ) : null}
+
+          {isStakeholder ? (
+            <button
+              type="button"
+              className="bg-gradient-to-r from-violet-400 to-violet-800 py-2 px-3 rounded-md text-white"
+              onClick={onPropose}
+            >
+              Propose
+            </button>
+          ) : null}
+        </div>
+      </div>
+      <div>
+        <p className='font-semibold text-xl mb-5'>CONNECTING YOUR WALLET TO A TESTNET IS A CRUCIAL STEP!</p>
+        <iframe
+          width="672"
+          height="378"
+          src="https://www.youtube.com/embed/tKMz3NAdIwg"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </div>
   )
